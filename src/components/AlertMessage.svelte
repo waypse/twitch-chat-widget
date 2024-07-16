d67bfea064e4f3d204af29eb9dc6980f5389aca1
fFrequence <ifrahfares@yahoo.fr>
2024-07-16 22:47:26 +0200

    added alert images

--- src/components/AlertMessage.svelte
+++ src/components/AlertMessage.svelte
@@ -1,6 +1,18 @@
 <script>
+  import newSubAnimation from "../assets/new-sub.txt?raw";
+
   export let message;
 </script>
 
+<div>
+  <img src={newSubAnimation} alt="newSubAnimation" />
+</div>
+
 <style>
+  div {
+    background-color: aqua;
+  }
+  img {
+    width: 400px;
+  }
 </style>
